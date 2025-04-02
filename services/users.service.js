dc9aad9e9ab38c40d293b04d3f1b44c7f532b7dc
Arun Devarajan <arun37dev@gmail.com>
2025-04-02 12:31:42 +0530

    add bcrypt

--- services/users.service.js
+++ services/users.service.js
@@ -1,11 +1,15 @@
 const { User } = require("../models/users.schema")
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 module.exports.createUser = async (data) => {
     const user = await User.findOne({ email: data.email });
     if(user) {
         throw new Error('User already registered')
     }
+    const hash = await bcrypt.hash(data.password, saltRounds)
+    data.password = hash;
     const newUser = new User(data);
     await newUser.save();
     return {message: 'Success'}
@@ -13,7 +17,8 @@ module.exports.createUser = async (data) => {
 
 module.exports.signIn = async (data) => {
     const user = await User.findOne({ email: data.email });
-    if(user && (user.password === data.password)) {
+    const isCorrectPassword = await bcrypt.compare(data.password, user.password)
+    if(user && isCorrectPassword) {
         const token = jwt.sign({ name: user.name, id: user._id }, process.env.secret, { expiresIn: '1d' });
         return { token, message: 'Success' }
     }
